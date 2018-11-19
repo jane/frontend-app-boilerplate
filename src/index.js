0e54b1f54b9220c52fe9e709ef5256b276d30346
zacanger <zac@zacanger.com>
2018-11-19 14:19:14 -0700

    Update deps

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import theme from 'jane-components/lib/theme'
 import Normalize from 'react-router-path-normalize'
-
 import App from './app'
-import './assets/global-styles'
+import { GlobalStyle } from './assets/global-styles'
 
 window._jane = global._jane || {}
 // eslint-disable-next-line no-undef
@@ -15,11 +14,14 @@ window._jane.version = process.env.APP_VERSION
 const renderApp = (A) => {
   ReactDOM.render(
     <ThemeProvider theme={theme}>
-      <Router>
-        <Normalize>
-          <A />
-        </Normalize>
-      </Router>
+      <React.Fragment>
+        <GlobalStyle />
+        <Router>
+          <Normalize>
+            <A />
+          </Normalize>
+        </Router>
+      </React.Fragment>
     </ThemeProvider>,
     document.querySelector('main')
   )
